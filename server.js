bfc0cc72c7001b086461afd55102f54fb1a00374
yangyunxin <850801584@qq.com>
2015-12-01 18:08:52 +0800

    function

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/incoming', secret: 'yangyunxin' })
 
-// function run_cmd(cmd, args, callback) {
-//   var spawn = require('child_process').spawn;
-//   var child = spawn(cmd, args);
-//   var resp = "";
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
  
-//   child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
-//   child.stdout.on('end', function() { callback (resp) });
-// }
+  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+  child.stdout.on('end', function() { callback (resp) });
+}
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
@@ -34,5 +34,5 @@ handler.on('issues', function (event) {
     event.payload.action,
     event.payload.issue.number,
     event.payload.issue.title);
-  //run_cmd('sh', ['./deploy-master.sh'], function(text){ console.log(text) });
+  run_cmd('sh', ['./deploy-master.sh'], function(text){ console.log(text) });
 });
